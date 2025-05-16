b6b23c69cb91dc6f448eceb536f4fe2c10284c71
Roy <inf.roycd@gmail.com>
2025-05-15 22:56:57 -0500

    chore: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: '3000-agnostic-contpaqi-nodejs-service',
+      name: '4000-agnostic-contpaqi-nodejs-service',
       script: 'dist/main.js',
       instances: 1,
       exec_mode: 'fork',
       watch: false,
       env: {
         NODE_ENV: 'development',
-        PORT: 3000,
+        PORT: 4000,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 4000,
       },
       error_file: './logs/error.log',
       out_file: './logs/output.log',
