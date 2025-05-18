3c9b0761ce465d12fd168d2aa988681cc67818ac
Roy <inf.roycd@gmail.com>
2025-05-18 12:10:21 -0500

    chore: update

--- src/features/video/video.controller.ts
+++ src/features/video/video.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import {
   UpserVideoDto,
   UpserVideoResponseDto,
   VideoIdDto,
 } from './dtos/upsert-video.dto';
 import { VideoService } from './video.service';
-
+@ApiBearerAuth()
 @Controller('videos')
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
